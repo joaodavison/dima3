1fe614c899321dfd1313585dd07d2d860315707d
peec <pcarmo@gmv.com>
2024-06-27 10:32:48 +0100

    Added console driver usage and adapted makefile

--- kernel.c
+++ kernel.c
@@ -1,5 +1,14 @@
+#include "console_driver/console.h"
+
 void kernel_main() {
     // Your kernel code here
+    console_init();
+    bsp_putchar('t');
+    bsp_putchar('e');
+    bsp_putchar('s');
+    bsp_putchar('t');
+    bsp_putchar('\n');
+
     while (1) {
         // Infinite loop to halt the kernel
     }
