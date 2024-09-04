12903dd185b556ab3227df79346c291700bf5e48
Luís Fernando Arcaro <luis.arcaro@embraer.com.br>
2024-09-04 12:19:24 -0300

    Refactored console driver and added wait for transmission conclusion.

--- console_driver/console.c
+++ console_driver/console.c
@@ -1,189 +1,93 @@
-// Assumed B_UART_FCR_FIFOE and  B_UART_FCR_FIFO64 are set
-
-#include <stdint.h>
-#define SerialRegisterBase (0x031d0000u)
-#define PcdSerialClockRate (1843200)
-#define PcdSerialBaudRate (115200u)
-
-#define BIT0 (1u)
-#define BIT1 (2u)
-#define BIT2 (4u)
-#define BIT3 (8u)
-#define BIT4 (16u)
-#define BIT5 (32u)
-#define BIT6 (64u)
-#define BIT7 (128u)
-#define BIT8 (256u)
-
-#define false (0u)
-#define true (1u)
-
-// 16550 UART register offsets and bitfields
-#define R_UART_RXBUF         0    // LCR_DLAB = 0
-#define R_UART_TXBUF         0    // LCR_DLAB = 0
-#define R_UART_BAUD_LOW      0    // LCR_DLAB = 1
-#define R_UART_BAUD_HIGH     1    // LCR_DLAB = 1
-#define R_UART_IER           1    // LCR_DLAB = 0
-#define R_UART_FCR           2
-#define   B_UART_FCR_FIFOE   BIT0
-#define   B_UART_FCR_FIFO64  BIT5
-#define R_UART_LCR           3
-#define   B_UART_LCR_DLAB    BIT7
-#define R_UART_MCR           4
-#define   B_UART_MCR_DTRC    BIT0
-#define   B_UART_MCR_RTS     BIT1
-#define R_UART_LSR           5
-#define   B_UART_LSR_RXRDY   BIT0
-#define   B_UART_LSR_TXRDY   BIT5
-#define   B_UART_LSR_TEMT    BIT6
-#define R_UART_MSR           6
-#define   B_UART_MSR_CTS     BIT4
-#define   B_UART_MSR_DSR     BIT5
-#define   B_UART_MSR_RI      BIT6
-#define   B_UART_MSR_DCD     BIT7
-
-unsigned char SerialPortReadRegister(unsigned int  Base, unsigned int  Offset) {
-  return *((unsigned char*)(Base + Offset));
-}
-
-void SerialPortWriteRegister(unsigned int  Base, unsigned int  Offset, unsigned char  Value) {
-  *((unsigned char*)(Base + Offset)) = Value;
-}
-
-unsigned int Tegra16550SerialPortWrite(unsigned char *Buffer, unsigned int NumberOfBytes) {
-  unsigned int  Result;
-  unsigned int  Index;
-  unsigned int  FifoSize;
-
-  if (NumberOfBytes == 0) {
-    // Flush the hardware
-
-    // Wait for both the transmit FIFO and shift register empty.
-    while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) != (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) {}
-
-    return 0;
-  }
-
-  // Compute the maximum size of the Tx FIFO
-  //if ((PcdGet8(PcdSerialFifoControl) & B_UART_FCR_FIFOE) != 0) {
-    //if ((PcdGet8(PcdSerialFifoControl) & B_UART_FCR_FIFO64) == 0) {
-      FifoSize = 16;
-    //} else {
-    //  FifoSize = PcdGet32(PcdSerialExtendedTxFifoSize);
-    //}
-  //}
-
-  Result = NumberOfBytes;
-  while (NumberOfBytes != 0) {
-    // Wait for the serial port to be ready, to make sure both the transmit FIFO
-    // and shift register empty.
-    while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) != (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) {
-    }
-
-    // while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & B_UART_LSR_TEMT) == 0);
-
-    // Fill then entire Tx FIFO
-    for (Index = 0; Index < FifoSize && NumberOfBytes != 0; Index++, NumberOfBytes--, Buffer++) {
-      // Write byte to the transmit buffer.
-      SerialPortWriteRegister(SerialRegisterBase, R_UART_TXBUF, *Buffer);
-    }
-  }
-
-  return Result;
-}
-
-unsigned int Tegra16550SerialPortRead(unsigned char *Buffer, unsigned int NumberOfBytes) {
-  unsigned int  Result;
-
-  for (Result = 0; NumberOfBytes-- != 0; Result++, Buffer++) {
-    // Wait for the serial port to have some data.
-    while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & B_UART_LSR_RXRDY) == 0) {}
-
-    // Read byte from the receive buffer.
-    *Buffer = SerialPortReadRegister(SerialRegisterBase, R_UART_RXBUF);
-  }
-
-  return Result;
-}
-
-void Tegra16550SerialPortSetAttributes(void) {
-  unsigned int   Divisor;
-  unsigned char   Lcr;
-  unsigned char   LcrData;
-  unsigned char   LcrParity = 0u;
-  unsigned char   LcrStop = 0u;
-  unsigned char   DataBits = 8u;
-
-  // Map 5..8 to 0..3
-  LcrData = (DataBits - 5u);
-  LcrParity = 0;
-  LcrStop = 0;
-
-  // Calculate divisor for baud generator
-  //    Ref_Clk_Rate / Baud_Rate / 16
-  Divisor = PcdSerialClockRate / (PcdSerialBaudRate * 16);
-  if ((PcdSerialClockRate % (PcdSerialBaudRate * 16)) >= PcdSerialBaudRate * 8) {
-    Divisor++;
-  }
-
-  // Configure baud rate
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, B_UART_LCR_DLAB);
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_BAUD_HIGH, (unsigned char)(Divisor >> 8));
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_BAUD_LOW, (unsigned char)(Divisor & 0xff));
-
-  // Clear DLAB and configure Data Bits, Parity, and Stop Bits.
-  // Strip reserved bits from line control value
-  Lcr = (unsigned char)((LcrParity << 3) | (LcrStop << 2) | LcrData);
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, (unsigned char)(Lcr & 0x3F));
-}
+#include "console.h"
 
 void console_init(void) {
-  unsigned int   Divisor;
-  unsigned int   CurrentDivisor;
 
-  // Calculate divisor for baud generator
-  //    Ref_Clk_Rate / Baud_Rate / 16
-  Divisor = PcdSerialClockRate / (PcdSerialBaudRate * 16);
-  if ((PcdSerialClockRate % (PcdSerialBaudRate * 16)) >= PcdSerialBaudRate * 8) {
-    Divisor++;
+  // Points module
+  volatile TEGRA_ModuleUart* module = TEGRA_REG_UARTA;
+
+  // Wait for the serial port to be ready
+  while ((module->LSR & (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) != (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) {}
+
+  // Calculates baud rate divisor
+  unsigned int divisor = TEGRA_UART_CLOCK_RATE / (TEGRA_UART_BAUD_RATE * 16u);
+  if ((TEGRA_UART_CLOCK_RATE % (TEGRA_UART_BAUD_RATE * 16u)) >= TEGRA_UART_BAUD_RATE * 8u) {
+    divisor++;
   }
 
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, (unsigned char)(SerialPortReadRegister(SerialRegisterBase, R_UART_LCR) | B_UART_LCR_DLAB));
-  CurrentDivisor  =  SerialPortReadRegister(SerialRegisterBase, R_UART_BAUD_HIGH) << 8;
-  CurrentDivisor |= (unsigned int)SerialPortReadRegister(SerialRegisterBase, R_UART_BAUD_LOW);
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, (unsigned char)(SerialPortReadRegister(SerialRegisterBase, R_UART_LCR) & ~B_UART_LCR_DLAB));
+  // Configures UART baud rate
+  module->LCR = TEGRA_UART_LCR_DLAB;
+  module->DLM = (unsigned char)(divisor >> 8);
+  module->DLL = (unsigned char)(divisor & 0xFFu);
 
-  // Wait for the serial port to be ready.
-  // Verify that both the transmit FIFO and the shift register are empty.
-  while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) != (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) {}
+  // Clears DLAB and configures UART line
+  module->LCR = (unsigned char)((TEGRA_UART_PARITY << 3) | (TEGRA_UART_STOP_BITS << 2) | ((TEGRA_UART_DATA_BITS - 5u) << 0));
 
-  // Configure baud rate
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, B_UART_LCR_DLAB);
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_BAUD_HIGH, (unsigned char)(Divisor >> 8));
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_BAUD_LOW, (unsigned char)(Divisor & 0xff));
+  // Enables and resets FIFOs
+  module->FCR = 0x0u;
+  module->FCR = (unsigned char)(TEGRA_UART_FCR_FIFOENABLE | TEGRA_UART_FCR_FIFO64);
 
-  // Clear DLAB and configure Data Bits, Parity, and Stop Bits.
-  // Strip reserved bits from PcdSerialLineControl
-  //SerialPortWriteRegister(SerialRegisterBase, R_UART_LCR, (unsigned char)(PcdGet8(PcdSerialLineControl) & 0x3F));
-  Tegra16550SerialPortSetAttributes();
+  // Configures to use FIFOs in polled mode
+  module->IER = 0x0u;
 
-  // Enable and reset FIFOs
-  // Strip reserved bits from PcdSerialFifoControl
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_FCR, 0x00);
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_FCR, (unsigned char)(B_UART_FCR_FIFOE | B_UART_FCR_FIFO64));
+  // Resets
+  module->MCR = 0x0u;
+}
 
-  // Set FIFO Polled Mode by clearing IER after setting FCR
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_IER, 0x00);
+void console_write(unsigned char *buffer, unsigned int size) {
+  unsigned int index;
 
-  // Put Modem Control Register(MCR) into its reset state of 0x00.
-  SerialPortWriteRegister(SerialRegisterBase, R_UART_MCR, 0x00);
+  // Points module
+  volatile TEGRA_ModuleUart* module = TEGRA_REG_UARTA;
+
+  // Loops through buffer size
+  while (size > 0u) {
+
+    // Waits for peripheral to be ready to transmit
+    while ((module->LSR & (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) != (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) {}
+
+    // Fills the transmission FIFO
+    for (index = 0u; index < TEGRA_UART_FIFO_SIZE && size != 0u; index++, size--, buffer++) {
+
+      // Writes byte to the transmit buffer
+      module->THR = *buffer;
+    }
+  }
+}
+
+unsigned int console_read(unsigned char *buffer, unsigned int size) {
+  unsigned int count;
+
+  // Points module
+  volatile TEGRA_ModuleUart* module = TEGRA_REG_UARTA;
+
+  // Iterates buffer bytes
+  for (count = 0u; count < size; count++) {
+
+    // Waits for peripheral to be ready to receive
+    while ((module->LSR & TEGRA_UART_LSR_RXREADY) == 0) {}
+
+    // Reads byte from the reception FIFO
+    *buffer = module->RBR;
+
+    // Advances buffer pointer
+    buffer++;
+  }
+
+  // Returns bytes read
+  return count;
 }
 
 void bsp_putchar(char c) {
-  // Wait for the serial port to be ready, to make sure both the transmit FIFO and shift register empty.
-  // while ((SerialPortReadRegister(SerialRegisterBase, R_UART_LSR) & (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) != (B_UART_LSR_TEMT | B_UART_LSR_TXRDY)) {}
-  // SerialPortWriteRegister(SerialRegisterBase, R_UART_TXBUF, c);
-  unsigned char *reg;
-  reg = (unsigned char*) 0x31d0000;
-  *reg = c;
+
+  // Points module
+  volatile TEGRA_ModuleUart* module = TEGRA_REG_UARTA;
+
+  // Waits for peripheral to be ready to transmit
+  while ((module->LSR & (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) != (TEGRA_UART_LSR_TEMT | TEGRA_UART_LSR_TXREADY)) {}
+
+  // Writes to transmission buffer
+  module->THR = c;
+
+  // Waits for peripheral to finish transmission
+  while ((module->LSR & TEGRA_UART_LSR_TEMT) != TEGRA_UART_LSR_TEMT) {}
 }
--- console_driver/console.h
+++ console_driver/console.h
@@ -1,7 +1,64 @@
 #ifndef CONSOLE_H_
 #define CONSOLE_H_
 
+// UART base address
+#define TEGRA_UARTA_BASE (0x03100000u)
+
+// UART register set
+#define TEGRA_REG_UARTA ((volatile TEGRA_ModuleUart*)TEGRA_UARTA_BASE)
+
+/**
+ * UART module.
+ */
+typedef struct {
+  union {
+    volatile unsigned char RBR; /* 0x0 - DLAB=0 - Read */
+    volatile unsigned char THR; /* 0x0 - DLAB=0 - Write */
+    volatile unsigned char DLL; /* 0x0 - DLAB=1 */
+  };
+  union {
+    volatile unsigned char IER; /* 0x1 - DLAB=0 */
+    volatile unsigned char DLM; /* 0x1 - DLAB=1 */
+  };
+  union {
+    volatile unsigned char IIR; /* 0x2 - Read */
+    volatile unsigned char FCR; /* 0x2 - Write */
+  };
+  volatile unsigned char LCR; /* 0x3 */
+  volatile unsigned char MCR; /* 0x4 */
+  volatile unsigned char LSR; /* 0x5 */
+  volatile unsigned char MSR; /* 0x6 */
+  volatile unsigned char SCR; /* 0x7 */
+} TEGRA_ModuleUart;
+
+#define TEGRA_UART_FCR_FIFOENABLE   (1u << 0)
+#define TEGRA_UART_FCR_FIFO64       (1u << 5)
+
+#define TEGRA_UART_LCR_DLAB         (1u << 7)
+
+#define TEGRA_UART_MCR_DTRC         (1u << 0)
+#define TEGRA_UART_MCR_RTSC         (1u << 1)
+
+#define TEGRA_UART_LSR_RXREADY      (1u << 0)
+#define TEGRA_UART_LSR_TXREADY      (1u << 5)
+#define TEGRA_UART_LSR_TEMT         (1u << 6)
+
+#define TEGRA_UART_MSR_CTS          (1u << 4)
+#define TEGRA_UART_MSR_DSR          (1u << 5)
+#define TEGRA_UART_MSR_RI           (1u << 6)
+#define TEGRA_UART_MSR_DCD          (1u << 7)
+
+#define TEGRA_UART_FIFO_SIZE        (16u)
+
+#define TEGRA_UART_CLOCK_RATE       (407347200u)
+#define TEGRA_UART_BAUD_RATE        (115200u)
+#define TEGRA_UART_DATA_BITS        (8u)
+#define TEGRA_UART_STOP_BITS        (0u)
+#define TEGRA_UART_PARITY           (0u)
+
 void console_init(void);
+void console_write(unsigned char *buffer, unsigned int size);
+unsigned int console_read(unsigned char *buffer, unsigned int size);
 void bsp_putchar(char c);
 
 #endif /* CONSOLE_H_ */
